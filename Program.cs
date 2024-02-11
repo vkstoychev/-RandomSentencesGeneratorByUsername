771a7550a670a9ab824bff657bbe7564c948cfb9
Vladimir Stoychev <50701769+vkstoychev@users.noreply.github.com>
2024-02-11 18:00:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Peter", "Michell", "Jane", "Steve" };
+            string[] places = { "Sofia", "Plovdiv", "Varna", "Burgas" };
+            string[] verbs = { "eats", "holds", "sees", "plays with", "brings" };
+            string[] nouns = { "stones", "cake", "apple", "laptop", "bikes" };
+            string[] adverbs = { "slowly", "diligently", "warmly", "sadly", "rapidly" };
+            string[] details = { "near the river", "at home", "in the park" };
+
+            Console.WriteLine("Hello, this is your first random-generator sentence: ");
+
+            while (true)
+            {
+                string randomName = GetRandomWord(names);
+                string randomPlace = GetRandomWord(places);
+                string randomVerb = GetRandomWord(verbs);
+                string randomNoun = GetRandomWord(nouns);
+                string randomAdverbs = GetRandomWord(adverbs);
+                string randomDetails = GetRandomWord(details);
+
+                string who = $"{randomName} from {randomPlace}";
+                string action = $"{randomAdverbs} {randomVerb} {randomNoun}";
+                string sentence = $"{who} {action} {randomDetails}.";
+
+                Console.WriteLine(sentence);
+                Console.WriteLine("Click [Enter] to generate a new one.");
+                Console.ReadLine();
+            }
+        }
+
+        static string GetRandomWord(string[] words)
+        {
+            Random random = new Random();
+            int randomIndex = random.Next(words.Length);
+            string word = words[randomIndex];
+            return word;
+        }
+    }
+}
